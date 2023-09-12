a2103e5cdd40da578533fe9525397ecbce6e66a7
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-09-11 23:28:05 -0400

    Solucionados errores para el build

--- src/components/CurrentSong.ts
+++ src/components/CurrentSong.ts
@@ -23,16 +23,20 @@ export const playCurrentSong = () => {
   const audio = document.getElementById('current-song-audio')
   const imgButtonPlay = document.getElementById('current-song-play-img')
 
-  if (audio?.paused) {
-    audio?.play()
-    imgButtonPlay.setAttribute('src', '/pause.svg')
-  } else {
-    audio?.pause()
-    imgButtonPlay.setAttribute('src', '/play.svg')
+  if (audio && audio instanceof HTMLAudioElement) {
+    if (audio.paused) {
+      audio?.play()
+      imgButtonPlay?.setAttribute('src', '/pause.svg')
+    } else {
+      audio?.pause()
+      imgButtonPlay?.setAttribute('src', '/play.svg')
+
+    }
   }
+
 }
 
 export const reloadButtonPlay = () => {
   const imgButtonPlay = document.getElementById('current-song-play-img')
-  imgButtonPlay.setAttribute('src', '/pause.svg')
+  imgButtonPlay?.setAttribute('src', '/pause.svg')
 }
